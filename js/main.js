8c86b4aeff8f7453a186c9553db0f0893a8f166f
aarongustafson <aaron@easy-designs.net>
2015-10-22 15:36:34 -0400

    JavaScript tweaks

--- js/main.js
+++ js/main.js
@@ -37,24 +37,24 @@
 	window.getActiveMQ = function()
 	{
 			// Build the watcher
-		var watcher = document.createElement('div'),
+		var $watcher = document.createElement('div'),
 			// alias getComputedStyle
 			computed = window.getComputedStyle,
 			// Regexp for removing quotes
 			re = /['"]/g;
 		
 		// set upt the watcher and add it to the DOM
-		watcher.setAttribute( 'id', 'getActiveMQ-watcher' );
-		watcher.style.display = 'none';
-		body.appendChild( watcher );
+		$watcher.setAttribute( 'id', 'getActiveMQ-watcher' );
+		$watcher.style.display = 'none';
+		document.body.appendChild( $watcher );
 		
 		// We’ll redefine this method the first time it’s run
 		// For old IE
-		if ( 'currentStyle' in watcher )
+		if ( 'currentStyle' in $watcher )
 		{
 			window.getActiveMQ = function()
 			{
-				return watcher.currentStyle['fontFamily'].replace( re, '' );
+				return $watcher.currentStyle['fontFamily'].replace( re, '' );
 			};
 		}
 		// For modern browsers
@@ -62,7 +62,7 @@
 		{
 			window.getActiveMQ = function()
 			{
-				return computed( watcher, null ).getPropertyValue( 'font-family' ).replace( re, '' );
+				return computed( $watcher, null ).getPropertyValue( 'font-family' ).replace( re, '' );
 			};
 		}
 		// For everything else
@@ -79,67 +79,59 @@
 
 	// --- CUSTOM CODE --- //
 
-    var html = document.getElementsByTagName('html')[0],
-		body = document.getElementsByTagName('body')[0],
-		page_classes = html.classList,
-		class_prefix = 'drawer-nav-';
+    if ( ! 'classList' in document.body ) { return; }
 
-	if ( ! "classList" in body )
-	{
-		return;
-	}
+    var $html = document.getElementsByTagName('html')[0],
+		page_classes = $html.classList,
+		$menu_opener = document.getElementById( 'nav-jump' ),
+		$menu_closer = document.getElementById( 'menu-close' ),
+		drawer_enabled_class = 'drawer-nav-enabled',
+		drawer_open_class = 'drawer-nav-open';
 
 	var toggleDrawerNav_running = false;
-	function toggleDrawerNav(e)
-	{
-		e.preventDefault();
+	function toggleDrawerNav( event ) {
+		event.preventDefault();
 
 		// check to make sure it’s not already running
-		if ( toggleDrawerNav_running )
-		{
-			return;
-		}
+		if ( toggleDrawerNav_running ) { return; }
 
 		// block any secondary requests until we’re done
 		toggleDrawerNav_running = true;
 
 		// Toggle the class
-		page_classes.toggle( class_prefix + 'open' );
+		page_classes.toggle( drawer_open_class );
 
 		// toggle the "running" indicator off once the animation is complete
 		// Duration: .5 seconds
 		setTimeout(function(){
 			toggleDrawerNav_running = false;
 		}, 500);
-
 	}
 
 	// Adjust the nav to slide in from the right if the browser window is "small"
 	window.watchResize(function(){
 
-		var currentMQ = window.getActiveMQ(),
-			menu_trigger = document.getElementById( 'nav-jump' ),
-			menu_closer = document.getElementById( 'menu-close' ),
-			enabled_class = class_prefix + 'enabled';
+		var current_MQ = window.getActiveMQ();
 
-		if ( currentMQ == 'small' &&
-			 ! page_classes.contains( enabled_class ) )
+		if ( current_MQ == 'small' &&
+			 ! page_classes.contains( drawer_enabled_class ) )
 		{
-			page_classes.add( enabled_class );
+			page_classes.add( drawer_enabled_class );
 			// handle toggles
-			menu_trigger.addEventListener( 'click', toggleDrawerNav, false );
-			menu_trigger.addEventListener( 'touchdown', toggleDrawerNav, false );
-			menu_closer.addEventListener( 'click', toggleDrawerNav, false );
-			menu_closer.addEventListener( 'touchdown', toggleDrawerNav, false );
+			$menu_opener.addEventListener( 'click', toggleDrawerNav, false );
+			$menu_opener.addEventListener( 'touchdown', toggleDrawerNav, false );
+			$menu_closer.addEventListener( 'click', toggleDrawerNav, false );
+			$menu_closer.addEventListener( 'touchdown', toggleDrawerNav, false );
 		}
-		else if ( classes.contains( enabled_class ) )
+		else if ( current_MQ != 'small' &&
+		          page_classes.contains( drawer_enabled_class ) )
 		{
-			page_classes.remove( enabled_class );
+			page_classes.remove( drawer_enabled_class );
 			// handle toggles
-			menu_trigger.removeEventListener( 'click', toggleDrawerNav, false );
-			menu_trigger.removeEventListener( 'touchdown', toggleDrawerNav, false );
-			menu_closer.removeEventListener( 'click', toggleDrawerNav, false );
-			menu_closer.removeEventListener( 'touchdown', toggleDrawerNav, false );
+			$menu_opener.removeEventListener( 'click', toggleDrawerNav, false );
+			$menu_opener.removeEventListener( 'touchdown', toggleDrawerNav, false );
+			$menu_closer.removeEventListener( 'click', toggleDrawerNav, false );
+			$menu_closer.removeEventListener( 'touchdown', toggleDrawerNav, false );
 		}
 
 	});
